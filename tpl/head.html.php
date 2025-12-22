f0adb3c6634f2a99f4d9b994ac3319825e2823ec
Gaido <luisfernandogaido@gmail.com>
2025-12-22 18:31:57 -0300

    Limpar projeto: favicon and manifest

--- tpl/head.html.php
+++ tpl/head.html.php
@@ -8,8 +8,8 @@ use templates\Gaido;
 <?php if ($this->favicon): ?>
     <link rel="icon" type="image/png" href="<?= SITE . $this->favicon ?>">
 <?php else: ?>
-    <link rel="icon" type="image/png" href="<?= SITE ?>tpl/img/chess-256.png">
+    <link rel="icon" type="image/png" href="<?= SITE ?>tpl/img/giroos-v1-256-white.png">
 <?php endif; ?>
 
-<link rel="apple-touch-icon" sizes="180x180" href="<?= SITE ?>tpl/img/chess-180-iphone.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?= SITE ?>tpl/img/giroos-v1-180-iphone.png">
 <link rel="manifest" href="<?= SITE ?>app/manifest.json?v=3">
