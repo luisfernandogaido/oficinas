d748a132c06f24e8c3e20b0881f28033e37dbd9a
Gaido <luisfernandogaido@gmail.com>
2025-12-26 18:35:26 -0300

    sem sucesso=Despersonificado.

--- app/usuarios/despersonifica.php
+++ app/usuarios/despersonifica.php
@@ -2,7 +2,7 @@
 include '../../def.php';
 try {
     Aut::despersonifica();
-    header('Location: ../index.php?sucesso=Despersonificado.');
+    header('Location: ../index.php');
 } catch (Exception $e) {
     header('Location: index.php?erro=' . urlencode($e->getMessage()));
 }
